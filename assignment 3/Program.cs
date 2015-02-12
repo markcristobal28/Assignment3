c01e72f1fe475af5a0a52a79e268bbacd942c6ca
markcristobal28 <mark_cristobal@hotmail.com>
2015-02-11 20:33:39 -0500

    created basevs c# project

---
+++ assignment 3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_3
+{
+    class Program
+    {
+        enum Menu
+        {
+            Burger =  1, 
+
+        }
+        static void Main(string[] args)
+        {
+            int foodMenu;
+            string prompt;
+            
+
+            Console.WriteLine("Q1. What do you wanna eat?: ");
+            Console.WriteLine("++++++++++++++++++++++++++++++++");
+            Console.WriteLine("1 - Burger");
+            Console.WriteLine("2 - Pizza");
+            Console.WriteLine("3 - Steak");
+            Console.WriteLine("4 - Fries");
+            Console.Write("Please make your selection (1 to 4): ");
+            prompt = Console.ReadLine();
+            Console.Write("\n");
+           foodMenu = Convert.ToInt32(prompt);
+
+        }
+    }
+}
